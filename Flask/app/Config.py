b5fda9a93dc9e3c4ea2ea53d9d38b6b7ad95a721
viktor2005666 <viktor.walravens@ugent.be>
2025-11-08 14:19:19 +0100

    .

---
+++ Flask/app/Config.py
@@ -0,0 +1,5 @@
+class Config:
+    SECRET_KEY = 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:[Group30ugent!]@db.rflzgpbuvokvlzaqrige.supabase.co:5432/postgres'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
